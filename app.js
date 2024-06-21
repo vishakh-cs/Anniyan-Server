3e61f3b5f8a13de5c0767fa56c9256ca655f80fe
vishakh-cs <143394085+vishakh-cs@users.noreply.github.com>
2024-06-21 18:13:28 +0530

    Fixed .gitignore

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+require('dotenv').config();
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CLIENT_URL,
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('socket is on');
+  
+  socket.on('chat', (message) => {
+    io.emit('chat', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('server connected on port 3000');
+});
