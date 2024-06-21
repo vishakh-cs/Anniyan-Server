66d0b2f19afc4f19a27922861c391fe82c5007a2
vishakh-cs <143394085+vishakh-cs@users.noreply.github.com>
2024-06-21 23:12:18 +0530

    nokkammm

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL,
-  },
+    origin: '*',
+  }
 });
 
 io.on('connection', (socket) => {
